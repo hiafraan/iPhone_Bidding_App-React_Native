37b30e0d557d3fb945399f3ff2a0545b5107c4fe
MrRaan <raanlance@gmail.com>
2023-01-11 11:01:47 +0530

    first commit

---
+++ components/ICard.js
@@ -0,0 +1,60 @@
+import { View, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { CircleButton, RectButton } from "./Button";
+import { DollarPrice, ITitle, SubInfo } from "./SubInfo";
+const ICard = ({ data }) => {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderBottomRightRadius: SIZES.font,
+          }}
+        />
+        <CircleButton imgUrl={assets.heart} top={10} right={10} />
+      </View>
+      <SubInfo />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <ITitle
+          title={data.name}
+          subTitle={data.condition}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <DollarPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default ICard;
---
+++ constants/assets.js
@@ -0,0 +1,37 @@
+import badge from "../assets/icons/badge.png";
+import dollar from "../assets/icons/dollar.png";
+import heart from "../assets/icons/heart.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+
+import logo from "../assets/images/logo.png";
+import iphone01 from "../assets/images/iphone01.png";
+import iphone02 from "../assets/images/iphone02.jpeg";
+import iphone03 from "../assets/images/iphone03.jpeg";
+import iphone04 from "../assets/images/iphone04.jpeg";
+import iphone05 from "../assets/images/iphone05.jpeg";
+
+import person01 from "../assets/images/person01.png";
+import person02 from "../assets/images/person02.png";
+import person03 from "../assets/images/person03.png";
+import person04 from "../assets/images/person04.png";
+
+export default {
+  badge,
+  dollar,
+  heart,
+  left,
+  search,
+
+  logo,
+  iphone01,
+  iphone02,
+  iphone03,
+  iphone04,
+  iphone05,
+
+  person01,
+  person02,
+  person03,
+  person04,
+};
